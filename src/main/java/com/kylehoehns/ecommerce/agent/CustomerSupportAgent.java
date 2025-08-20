418b4509e3d7afd695519ae1a72a40d56106229c
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2025-08-20 14:37:59 -0500

    full interaction

--- src/main/java/com/kylehoehns/ecommerce/agent/CustomerSupportAgent.java
+++ src/main/java/com/kylehoehns/ecommerce/agent/CustomerSupportAgent.java
@@ -31,9 +31,10 @@ public class CustomerSupportAgent {
         this.inventoryService = inventoryService;
     }
 
-    record OrderAdjustmentResponse(String orderId, OperationType operationType, String userMessage) {
+    record OrderAdjustmentResponse(Order order, OperationType operationType, String userMessage) {
     }
-    record OrderAdjustmentResponse2(String orderId, OperationType operationType, String userMessage) {
+
+    record OrderAdjustmentResponse2(Order order, OperationType operationType, String userMessage) {
     }
 
     enum OperationType {
@@ -44,43 +45,78 @@ public class CustomerSupportAgent {
 
     @AchievesGoal(description = "Handles the refund or replacement of an order for a customer")
     @Action
-    OrderAdjustmentResponse2 completeAdjustment(OrderAdjustmentResponse orderAdjustmentResponse, OrderSearchResult searchResult) {
+    OrderAdjustmentResponse2 completeAdjustment(OrderAdjustmentResponse orderAdjustmentResponse,
+                                                OrderSearchResult searchResult,
+                                                CustomerResponse customerResponse) {
         if (!searchResult.orderExists()) {
             // TODO: Maybe this just throws?
-            return new OrderAdjustmentResponse2(orderAdjustmentResponse.orderId(), OperationType.UNKNOWN, "An order with that ID does not exist");
+            return new OrderAdjustmentResponse2(orderAdjustmentResponse.order(), OperationType.UNKNOWN, "An order with that ID does not exist");
         }
 
-        return new OrderAdjustmentResponse2(orderAdjustmentResponse.orderId(), orderAdjustmentResponse.operationType(), orderAdjustmentResponse.userMessage());
+        return new OrderAdjustmentResponse2(orderAdjustmentResponse.order(), orderAdjustmentResponse.operationType(), customerResponse.message());
     }
 
-    @Action( pre = "hasInventory")
+    record CustomerResponse(String message) { }
+
+    @Action
+    CustomerResponse createCustomerResponse(OrderAdjustmentResponse oar, OperationContext context, ParsedRequest parsedRequest) {
+        var message = context.promptRunner()
+            .createObject("""
+                You are a customer support representative letting a customer know that their order has been %s
+                
+                Here is information about their order:
+                %s
+                
+                When they originally submitted the request, they had the following sentiment: %s
+                
+                They wanted the order to be: %s
+                The order was actually: %s
+                
+                If the user wanted a replacement, but we issued a refund. It was due to inventory issues.
+                
+                Please create a message for them in the proper tone, taking into effect their original tone and letting them know about their order in 100 words or less.
+                
+                Orders usually ship within 1-3 business days.
+                If it is a refund, funds will be back on their card within 1 business day.
+                
+                Our company name is ACME, co if you refer to the company.
+                """.formatted(oar.operationType(), oar.order(), parsedRequest.sentiment(), parsedRequest.intent(), oar.operationType()), String.class);
+
+        return new CustomerResponse(message);
+    }
+
+
+    @Action(pre = "shouldReplace")
     OrderAdjustmentResponse processReplacement(OrderSearchResult searchResult) {
         var order = searchResult.order();
         var replacement = orderService.processReplacement(order.id(), order.sku(), order.sku(), 1, BigDecimal.ONE);
-        return new OrderAdjustmentResponse(replacement.id(), OperationType.REPLACE, "We replaced it dude");
+        return new OrderAdjustmentResponse(replacement, OperationType.REPLACE, "We replaced it dude");
     }
 
-    @Action(pre = "doesNotHaveInventory")
+    @Action(pre = "shouldRefund")
     OrderAdjustmentResponse processRefund(OrderSearchResult searchResult) {
         var order = searchResult.order();
         var refundMsg = orderService.processRefund(order.id(), order.sku(), 1, BigDecimal.ONE);
-        return new OrderAdjustmentResponse(order.id(), OperationType.REFUND, refundMsg);
+        return new OrderAdjustmentResponse(order, OperationType.REFUND, refundMsg);
     }
 
-    @Condition(name = "hasInventory")
-    public boolean hasInventory(OrderSearchResult orderSearchResult) {
-        return inventoryService.getQuantity(orderSearchResult.order().sku()) > 0;
+    @Condition(name = "shouldReplace")
+    public boolean shouldReplace(ParsedRequest parsedRequest, OrderSearchResult searchResult) {
+        var wantsReplacement = Intent.REPLACEMENT.equals(parsedRequest.intent());
+        boolean hasInventory = inventoryService.getQuantity(searchResult.order().sku()) > 0;
+        return hasInventory && wantsReplacement;
     }
 
-    @Condition(name = "doesNotHaveInventory")
-    public boolean doesNotHaveInventory(OrderSearchResult orderSearchResult) {
-        return inventoryService.getQuantity(orderSearchResult.order().sku()) <= 0;
+    @Condition(name = "shouldRefund")
+    public boolean shouldRefund(ParsedRequest parsedRequest, OrderSearchResult searchResult) {
+        boolean hasInventory = inventoryService.getQuantity(searchResult.order().sku()) > 0;
+        return Intent.REFUND.equals(parsedRequest.intent()) || !hasInventory;
     }
 
     record OrderSearchResult(Order order, boolean orderExists) {
     }
 
-    @Action( post = { "hasInventory", "doesNotHaveInventory"} )
+    @Action(post = {"shouldRefund", "shouldReplace"})
     OrderSearchResult retrieveOrder(ParsedRequest parsedRequest) {
         log.info("Retrieving order {}", parsedRequest);
         return orderService.getOrder(parsedRequest.orderId())
